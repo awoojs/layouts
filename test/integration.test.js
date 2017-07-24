dc96c007218ad22527cb0c4c4ef10326e9960516
olivia <olivia@fastmail.com>
2017-07-24 17:03:06 +0200

    adjust tests

--- test/integration.test.js
+++ test/integration.test.js
@@ -2,17 +2,25 @@ const test = require('ava')
 const weh = require('@weh/weh')
 const plugin = require('../lib/plugin')
 
+const layout = (file, files) =>
+`
+h${file.contents}
+`
+
 test('plugin works', async t => {
   const files = [
     {
-      contents: 'sample contents'
+      contents: 'ello',
+      metadata: {
+        layout: 'layout'
+      }
     }
   ]
 
   const res = await weh.integration(async site => {
-    site.use(plugin, { contents: 'aaa' })
+    site.use(plugin, {layouts: {layout}})
     return site
   }, files)
 
-  t.is(res.files[0].contents, 'aaa')
+  t.is(res.files[0].contents, 'hello')
 })
